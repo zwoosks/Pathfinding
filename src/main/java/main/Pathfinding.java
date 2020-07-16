cab3474c828fcefc98fbe272c2bd5ea517fc4b7b
zwoosks <zwoosks@DESKTOP-F9RCAOR>
2020-07-16 20:37:53 +0200

    Added simple commands, a missing plugin.yml file, an Util class, fixed dependencies and removed test and resetTest functions from main class

--- src/main/java/main/Pathfinding.java
+++ src/main/java/main/Pathfinding.java
@@ -1,76 +1,33 @@
 package main;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import pathfinding.AStar;
-
-public class Pathfinding extends JavaPlugin implements Listener
-{
+public class Pathfinding extends JavaPlugin implements Listener {
+	
+	public static Location lc1;
+	public static Location lc2;
 	
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
 		System.out.println("[" + this.getDescription().getName() + "] v" + this.getDescription().getVersion() + " enabled!");
-		
-		test();
+		lc1 = null;
+		lc2 = null;
+		registerCommands();
 	}
 	
 	@Override
-	public void onDisable()
-	{
-		//resetTest();
-		
+	public void onDisable() {
 		System.out.println("[" + this.getDescription().getName() + "] v" + this.getDescription().getVersion() + " disabled!");
 	}
 	
-	// To remove
-	
-	Location[] path;
-	Material[] mat;
-	Byte[] data;
- 	@SuppressWarnings("deprecation")
-	public void test()
-	{
- 		
- 		
-		Location start = new Location(Bukkit.getWorlds().get(0), 0, 4, 0);
-		Location end = new Location(Bukkit.getWorlds().get(0), 20, 4, 0);
-		
-		AStar a = new AStar(start, end, 10000, true, 5);
-		path = a.getPath();
-		
-		mat = new Material[path.length];
-		data = new Byte[path.length];
-		
-		for(int i = 0; i < path.length; i++)
-		{
-			mat[i] = path[i].getBlock().getType();
-			data[i] = path[i].getBlock().getData();
-			
-			path[i].getBlock().setType(Material.GLASS);
-		}
-		
-		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-			@Override
-			public void run()
-			{
-				resetTest();
-			}
-		}, 80L);
-	}
-	
-	@SuppressWarnings("deprecation")
-	public void resetTest()
-	{
-		for(int i = 0; i < path.length; i++)
-		{
-			path[i].getBlock().setType(mat[i]);
-			path[i].getBlock().setData(data[i]);
-		}
+	private void registerCommands() {
+		CommandExecutor ce = new CommandManager(this);
+		getCommand("set1").setExecutor(ce);
+		getCommand("set2").setExecutor(ce);
+		getCommand("pathfinding").setExecutor(ce);
 	}
 	
 }
